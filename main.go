26c4905df173ba39fd6fdc9ba43dc8a2ea2fe2ed
Emilia Glibowska <emilia.glibowska@elmark.com.pl>
2025-03-22 07:06:05 +0100

    Added database connection

--- main.go
+++ main.go
@@ -1,17 +1,28 @@
 package main
 
 import (
-	database "github.com/TotallyNotLirgo/back-seat-boys/src/services/database"
 	http "net/http"
+
+	database "github.com/TotallyNotLirgo/back-seat-boys/src/services/database"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
 type Server struct {
-	database *database.ExampleDatabase
+	database *database.Database
 }
 
 func main() {
+	conn, err := gorm.Open(
+		sqlite.Open("test.db"),
+		&gorm.Config{},
+	)
+	if err != nil {
+		panic("failed to connect database")
+	}
+	conn.AutoMigrate(&database.User{})
 	server := Server{
-		database: &database.ExampleDatabase{},
+		database: &database.Database{Connection: conn},
 	}
 	http.HandleFunc("POST /api/login", server.login)
 	http.HandleFunc("POST /api/register", server.register)
